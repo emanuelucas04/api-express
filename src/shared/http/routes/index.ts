ae5345c1fb16748141aef8fc43b1eb268612c8ba
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-10-30 13:50:27 -0300

    Add roles routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,12 @@
-import { AppError } from '@shared/errors/AppError'
+import { rolesRouter } from '@roles/http/routes/roles.routes'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
-  throw new Error('Acesso negado')
   return response.json({ message: 'Olá Dev!' })
 })
+
+routes.use('/roles', rolesRouter)
 
 export { routes }

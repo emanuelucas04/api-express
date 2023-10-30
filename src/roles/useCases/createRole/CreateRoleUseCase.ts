eb25248ecda473cf0c54185b43b5cc58dbb6669a
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-10-30 17:04:23 -0300

    Config singleton in RolesRepository

--- src/roles/useCases/createRole/CreateRoleUseCase.ts
+++ src/roles/useCases/createRole/CreateRoleUseCase.ts
@@ -1,4 +1,6 @@
+import { Role } from '@roles/entities/Role'
 import { RolesRepository } from '@roles/repositories/RolesRepository'
+import { AppError } from '@shared/errors/AppError'
 
 type CreateRoleDTO = {
   name: string

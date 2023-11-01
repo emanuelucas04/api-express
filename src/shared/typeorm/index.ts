ccd614347706e4f051f3d7a6cbb63cad92f64261
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-11-01 10:24:09 -0300

    Refactor role entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1698840084404 } from './migrations/1698840084404-CreateRolesTable'
+import { Role } from '@roles/entities/Role'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1698840084404],
 })

5a8e3bb082cf6caa0b77023eaff7a42a23838e11
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-11-01 09:35:52 -0300

    Create migration CreateRolesTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1698840084404 } from './migrations/1698840084404-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1698840084404],
 })

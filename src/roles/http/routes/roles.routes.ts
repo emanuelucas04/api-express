a0ace93c0b0b255081caa918af1467e7193096dc
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-11-02 13:32:48 -0300

    Create useCase deleteRole

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -3,6 +3,7 @@ import { createRolesController } from '@roles/useCases/createRole'
 import { listRolesController } from '@roles/useCases/listRoles'
 import { showRoleController } from '@roles/useCases/showRole'
 import { updateRoleController } from '@roles/useCases/updateRole'
+import { deleteRoleController } from '@roles/useCases/deleteRole'
 
 const rolesRouter = Router()
 
@@ -20,6 +21,10 @@ rolesRouter.get('/:id', (request, response) => {
 
 rolesRouter.put('/:id', (request, response) => {
   return updateRoleController.handler(request, response)
+})
+
+rolesRouter.delete('/:id', (request, response) => {
+  return deleteRoleController.handler(request, response)
 })
 
 export { rolesRouter }

c15e5d162be1d2c17832bd7f4a805e8e4d546345
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-11-04 16:56:54 -0300

    Create migration CreateUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1698840084404 } from './migrations/1698840084404-CreateRolesTable'
 import { Role } from '@roles/entities/Role'
+import { CreateUsersTable1699125619175 } from './migrations/1699125619175-CreateUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1698840084404],
+  migrations: [CreateRolesTable1698840084404, CreateUsersTable1699125619175],
 })

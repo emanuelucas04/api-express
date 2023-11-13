f33b55470d4d600008c395a1bc4c9adcbb5b28d6
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-11-13 10:14:31 -0300

    Add CreateLoginController

---
+++ src/users/useCases/createLogin/CreateLoginController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+import { CreateLoginUseCase } from './CreateLoginUseCase'
+import { instanceToInstance } from 'class-transformer'
+
+export class CreateLoginController {
+  async handler(request: Request, response: Response): Promise<Response> {
+    const createLoginUseCase = container.resolve(CreateLoginUseCase)
+    const { email, password } = request.body
+    const { user, token } = await createLoginUseCase.execute({
+      email,
+      password,
+    })
+    return response.status(201).json(instanceToInstance({ user, token }))
+  }
+}

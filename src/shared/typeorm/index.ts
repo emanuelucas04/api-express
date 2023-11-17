a3376c69ad90f6411851e2933dabb7b82535f0f1
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-11-17 10:39:30 -0300

    Create migration CreateRefreshTokensTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,6 +4,7 @@ import { User } from '@users/entities/User'
 import { CreateRolesTable1698840084404 } from './migrations/1698840084404-CreateRolesTable'
 import { CreateUsersTable1699125619175 } from './migrations/1699125619175-CreateUsersTable'
 import { AddRoleIdToUsersTable1699359690651 } from './migrations/1699359690651-AddRoleIdToUsersTable'
+import { CreateRefreshTokensTable1700227010543 } from './migrations/1700227010543-CreateRefreshTokensTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
@@ -13,5 +14,6 @@ export const dataSource = new DataSource({
     CreateRolesTable1698840084404,
     CreateUsersTable1699125619175,
     AddRoleIdToUsersTable1699359690651,
+    CreateRefreshTokensTable1700227010543,
   ],
 })
